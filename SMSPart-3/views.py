52088ab6ebf3230819619649c879c495d0d7c832
Sonam Gautam <121800288+gautamsonam011@users.noreply.github.com>
2023-09-12 17:00:40 +0530

    Update views.py

--- SMSPart-3/views.py
+++ SMSPart-3/views.py
@@ -8,11 +8,18 @@ def get_course_wise_student_details(request):
     c=Course.objects.all()
     d1={'courses':c}
     if request.method=='GET':
+        c=Course.objects.get(id=1)
+        d1['cid']=c.id
+        d1['cname']=c.name
+        allstu=c.students.all()
+        d1['students']=allstu
         resp=render(request,'SMS/course.html',context=d1)
         return resp
     elif request.method=='POST':
         course_id=int(request.POST.get('courses',0))
         c=Course.objects.get(id=course_id)
+        d1['cid']=c.id
+        d1['cname']=c.name
         allstu=c.students.all()
         d1['students']=allstu
         resp=render(request,'SMS/course.html',context=d1)
